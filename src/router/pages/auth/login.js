0f0a5095693c5421f9cd6271baf291cf58315afe
parham-snk <psanjanaki@gmail.com>
2024-12-19 01:00:37 +0330

    s

--- src/router/pages/auth/login.js
+++ src/router/pages/auth/login.js
@@ -4,27 +4,47 @@ import { Link, NavLink } from "react-router";
 import axios from "axios";
 
 const Register = () => {
+    const [username, setUsername] = useState()
+    const [mail, setMail] = useState()
+    const [password, setPassword] = useState()
+    const [remmember, setRemmember] = useState(false)
 
-    const formAPI=()=>{
-        const form=new FormData(document.querySelector('form'))
+
+    const formAPI = () => {
+
+        const form = new FormData()
+        form.append('username', username)
+        form.append("password", password)
+        form.append('remmember', remmember)
         console.log(form)
-
-        // axios.post("",{form}).then(()=>{
-
-        // })
+        // fetch("http://localhost:8080/auth/login",{
+        //     method:"POST",
+        //     body:{username,password,remmember},
+        //     headers:{
+        //         'Content-Type': 'application/json',
+        //     }
+        // }).then()
+        axios.post('http://localhost:8080/auth/login', {username,password,remmember}).then().catch(err => console.log(err))
     }
     return (
         <div className="d-flex  justify-content-center align-items-center " style={{ height: "100vh" }}>
             <form className="d-flex flex-column w-75 justify-content-center align-items-start shadow px-4 py-5 rounded-3">
                 <label for="username">username : </label>
-                <input type="text" name="username" id="username" placeholder="username" className="w-100 p-1 my-1"  />
+                <input type="text" name="username" id="username" placeholder="username" className="w-100 p-1 my-1" onChange={e => {
+                    setUsername(e.target.value)
+                }} />
                 <label for="password" className="mt-3">password : </label>
-                <input type="password" name="password" id="password" placeholder="password" className="w-100 p-1 my-1"  />
+                <input type="password" name="password" id="password" placeholder="password" className="w-100 p-1 my-1" onChange={e => {
+                    setPassword(e.target.value)
+                }} />
 
-                <div className="d-flex flex-row my-3 pointer-event" style={{cursor:"pointer"}}>
-                    <input type="checkbox" name="remember" id="remember" style={{cursor:"pointer"}}/> <label style={{cursor:"pointer"}} for="remember" className="ms-2">remember me</label>
+                <div className="d-flex flex-row my-3 pointer-event" style={{ cursor: "pointer" }}>
+                    <input type="checkbox" name="remember" id="remember" style={{ cursor: "pointer" }} onClick={e => {
+                        e.target.checked ? setRemmember(true) : setRemmember(false)
+                    }} /> <label style={{ cursor: "pointer" }} for="remember" className="ms-2">remember me</label>
                 </div>
                 <br />
+                <input type="submit" value="submit" />
                 <div className="btn btn-outline-primary w-100 my-4" onClick={formAPI} >
                     login
                 </div>
--- src/router/pages/auth/register.js
+++ src/router/pages/auth/register.js
@@ -1,25 +1,34 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router";
+import axios from "axios";
+
 
 const Register = () => {
+    const [name, setName] = useState()
+    const [username, setUsername] = useState()
+    const [mail, setMail] = useState()
+    const [password, setPassword] = useState()
 
     const formAPI = async () => {
-        const form = FormData(document.querySelector('form'))
-        console.log(form)
+        axios.post('http://localhost:8080/auth/register', { name, username, mail, password })
     }
     return (
         <div className="d-flex  justify-content-center align-items-center " style={{ height: "100vh" }}>
             <form className="d-flex flex-column w-75 justify-content-center align-items-start shadow px-4 py-5 rounded-3">
 
-                <label for="mail">mail : </label>
-                <input type="text" name="mail" id="mail" placeholder="mail" className="w-100 p-1 my-1" />
+                <label for="name" className="mt-3">name : </label>
+                <input type="text" name="name" id="name" placeholder="name" onChange={e=>setName(e.target.value)} className="w-100 p-1 my-1" />
+
 
                 <label for="username" className="mt-3">username : </label>
-                <input type="text" name="username" id="username" placeholder="username" className="w-100 p-1 my-1" />
+                <input type="text" name="username" id="username" placeholder="username" onChange={e=>setUsername(e.target.value)} className="w-100 p-1 my-1" />
+
+                <label for="mail">mail : </label>
+                <input type="text" name="mail" id="mail" placeholder="mail" onChange={e=>setMail(e.target.value)} className="w-100 p-1 my-1" />
+
 
                 <label for="password" className="mt-3">password : </label>
-                <input type="password" name="password" id="password" placeholder="password" className="w-100 p-1 my-1" />
-
+                <input type="password" name="password" id="password" placeholder="password" onChange={e=>setPassword(e.target.value)} className="w-100 p-1 my-1" />
 
                 <br />
                 <div className="btn btn-outline-primary w-100 my-4" onClick={formAPI} >
